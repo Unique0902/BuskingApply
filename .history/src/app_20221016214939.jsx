6bc2b2b64bd9e99a0c568e268e83acb3fc7da198
mook <rla0591@gmail.com>
2022-10-16 22:00:42 +0900

    first version

---
+++ .history/src/app_20221016214939.jsx
@@ -0,0 +1,9 @@
+import './app.css';
+import SearchResult from './components/searchResult/searchResult';
+import Page_num_screen from './components/page_num_screen/page_num_screen';
+
+function App() {
+  return <h1>Hello :) </h1>;
+}
+
+export default App;
---
+++ .history/src/index_20221016214705.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './app';
+import AuthService from './service/auth_service';
+import PlaylistRepository from './service/playlist_repository';
+import UserRepository from './service/userRepository';
+import BuskingRepository from './service/buskingRepository';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const authService = new AuthService();
+const userRepository = new UserRepository();
+const playlistRepository = new PlaylistRepository();
+const buskingRepository = new BuskingRepository();
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='apply'>
+          <Route
+            path=':userId'
+            element={
+              <App
+                buskingRepository={buskingRepository}
+                userRepository={userRepository}
+                playlistRepository={playlistRepository}
+                ipService={ipService}
+              />
+            }
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
